1d780e32ec40ca5a5e12798267f9399d14b6a901
airsdream <airsdream@163.com>
2020-09-08 06:07:45 +0800

    V1.0 完成第一版模拟分析程序

--- main.py
+++ main.py
@@ -1,30 +1,107 @@
 from repair import GetRepairDate, GetRepairTime
+import random
+import numpy as np
+from matplotlib import pyplot as plt
 
-cycle = 10
-times = 2
-duration = 92
+cycle = 10000  #循环模拟次数
+duration = 92   #每次模拟运营时长 （天）
 
-on_business = []
+times = 1   #车均维修次数
+random_repair_times = True #是否开启随机维修次数
+min_repair_times = 1    #随机最小维修次数
+max_repair_times = 3    #随机最大维修次数
+
+min_repair_days = 1 #最短维修时长（天）
+max_repair_days = 3 #最长维修时长（天）
+
+average_mileage = 13.5    #每日车均骑行里程 km
+random_riding_mileage = True   #是否开启随机每日车均骑行里程
+min_mileage = 10.0   #随机最小每日车均骑行里程 km
+max_mileage = 15.0  #随机最大每日车均骑行里程 km
+
+# 生成随机的维修日期及维修时长 #
+
+repair_dates = []   #随机维修日期列表
+repair_times = []   #一一对应的随机维修时长列表
+repairs = []        #每次循环维修车辆次数的列表
 
 for i in range(cycle):
-    on_business.append(GetRepairDate(duration,times))
+    if random_repair_times:
+        times = random.randint(min_repair_times,max_repair_times)
+        repairs.append(times)
+    repair_dates.append(GetRepairDate(duration,times))
+    repair_times.append(GetRepairTime(min_repair_days,max_repair_days,times))
+    for j in range(times):
+        if j < times-1:
+            while repair_dates[i][j] + repair_times[i][j] >= repair_dates[i][j+1]:  #检查维修日期加上维修时长超过下一次维修日期的数据
+                #print('维修日期不合理',repair_dates[i],repair_times[i],'->',end='') #打印维修日期加上维修时长超过下一次维修日期的数据
+                repair_dates[i] = GetRepairDate(duration,times) #重新生成维修日期
+                #print(repair_dates[i],repair_times[i])  #打印新成圣的维修日期及上一次生成的维修时长
+        else:
+            if repair_dates[i][j] + repair_times[i][j] >= duration:
+                #print('最后一次维修超出运营期限', repair_dates[i], repair_times[i],'->',end='')
+                repair_times[i][j] = duration - repair_dates[i][j]
+                #print(repair_times[i][j])
+#print(repair_dates)
+#print(repair_times)
 
-print(on_business)
-# a = GetRepairDate(92,times)
-# b = GetRepairTime(1,7)
-# for i in range(times):
-#     if a[i] <= 31:
-#         Oct += 1
-#     elif a[i] <= 61:
-#         Nov += 1
-#     else:
-#         Dec +=1
-#     sum += 1
-#
-# OctPer = Oct / sum * 100
-# NovPer = Nov / sum * 100
-# DecPer = Dec / sum * 100
-#
-# print('Oct = %d , Nov = %d , Dec = %d'%(Oct,Nov,Dec))
-# print('Oct = %.2f %%, Nov = %.2f %%, Dec = %.2f %%'%(OctPer,NovPer,DecPer))
-# print(b)
+# 计算运营的时长 #
+
+on_business = []    #每次投放运营，该次运营的时长（天）
+business_days = []  #所有循环模拟中，每次循环的平均运营时长列表
+
+for i in range(cycle):
+    last_date = duration
+    on_business = []
+    for j in range(len(repair_dates[i])-1,-1,-1):
+        on_business.append(last_date - repair_dates[i][j] - repair_times[i][j])
+        last_date = repair_dates[i][j]
+    on_business.append(last_date)
+    on_business.reverse()
+    business_days.append(on_business)
+#print(business_days)
+
+# 计算每次循环中，每次投放运营的里程 #
+
+mileage_per_cycle = []
+
+for i in range(cycle):
+    mileage_per_day = []
+    for j in range(len(business_days[i])):
+        if random_riding_mileage:
+            average_mileage = random.uniform(min_mileage,max_mileage)
+        average_mileage = round(average_mileage,3)
+        mileage_per_day.append(business_days[i][j] * average_mileage)
+    mileage_per_cycle.append(mileage_per_day)
+#print(mileage_per_cycle)
+
+
+# 计算每次循环的累计平均运营里程 #
+
+success = 0 #累计车均里程达到500km以上的次数
+
+accumulative_average_mileage_per_cycle = []
+for i in range(cycle):
+    accumulative_average_mileage_per_cycle.append(np.average(mileage_per_cycle[i]))
+    if accumulative_average_mileage_per_cycle[i] >= 500:
+        success += 1
+#print(accumulative_average_mileage_per_cycle)
+
+
+if random_repair_times:
+    times = np.average(repairs)
+
+print('累计模拟循环 = ',cycle)
+print('平均维修次数 = ',times)
+print('成功达到500km目标的概率 = %.2f %%'%(success/cycle*100))
+
+# 划分横坐标分度值
+
+bins = []
+for i in range(0,(int(max(accumulative_average_mileage_per_cycle)) // 100 + 1) * 100 + 1,10):
+    bins.append(i)
+
+# 绘制直方图
+
+#plt.hist(accumulative_average_mileage_per_cycle, bins= bins)
+#plt.show()
--- repair.py
+++ repair.py
@@ -1,13 +1,25 @@
 import random
 
-def GetRepairTime (min_repair_time,max_repair_time):
-    repair_time = random.randint(min_repair_time,max_repair_time)
+def GetRepairTime (min_repair_time,max_repair_time,times):
+    repair_time = []
+    for i in range(times):
+        repair_time.append(random.randint(min_repair_time,max_repair_time))
     return repair_time
 
-def GetRepairDate (duration,times):
+def GetRandomRepairDate (duration,times):
     repair_date = []
     for i in range(times):
         repair_date.append(random.randint(1,duration))
-    repair_date.sort()
     return repair_date
+
+def GetRepairDate (duration,times): #在一段时间内随机挑选指定数量的检修日期
+    repair_date = GetRandomRepairDate(duration,times)
+    while len(set(repair_date)) != len(repair_date):    #检查是否有重复日期
+        #print('repeat ',' ',repair_date,end='->') #遇到重复的打印出来
+        del repair_date[:]  #删除这次生成的检修日期
+        repair_date = GetRandomRepairDate(duration,times)
+        #print(repair_date) #打印重新生成修正后的日期
+    repair_date.sort()  #从小到大排序
+    return repair_date
+
 
